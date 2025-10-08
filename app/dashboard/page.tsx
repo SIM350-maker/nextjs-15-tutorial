76e35ace58ba508d5c6807bb434e8e514bf3238f
SIM350-maker <simonipash@gmail.com>
2025-10-08 16:34:41 +0300

    finished code

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,52 @@
+import { BlogPostCard } from "@/components/general/BlogpostCard";
+import { buttonVariants } from "@/components/ui/button";
+import { prisma } from "@/utils/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+
+async function getData(userId: string){
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const data = await prisma.blogPost.findMany({
+        where: {
+            authorId:  userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+    return data;
+}
+    
+
+export default async function DashboardRoute(){
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return (
+            <div>
+                <h2 className="text-xl font-medium">You must be logged in to view your blog articles.</h2>
+            </div>
+        );
+    }
+
+    const data = await getData(user.id);
+
+    return(
+        <div>
+            <div className="flex items-center justify-between mb-4">
+                <h2 className="text-xl font-medium">Your Blog Articles</h2>
+
+                <Link className={buttonVariants()} href="/dashboard/create">
+                Create Post
+                </Link>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-4">
+                {data.map((item) => (
+                    <BlogPostCard data={item} key={item.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+
+      domains: ['th.bing.com'],
+
+    remotePatterns:[
+      {
+        hostname:'encrypted-tbn0.gstatic.com',
+        protocol:"https",
+        port:"",
+      },
+      {
+        hostname:'lh3.googleusercontent.com',
+        protocol:"https",
+        port:"",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
